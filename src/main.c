f9d87de38564351109370461f0d1a13d8be720b0
VS-123 <>
2026-03-27 09:10:53 +0000

    main.c: prepare to make ants

--- src/main.c
+++ src/main.c
@@ -1,5 +1,9 @@
-#include <raylib.h>
 #include <stdio.h>
+#include <time.h>
+
+#define YSTAR_IMPLEMENTATION
+#include "ystar.h"
+#include <raylib.h>
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 800
@@ -28,17 +32,26 @@ struct ant_t
 int
 main (void)
 {
+	uint64_t seed = time (NULL);
+
 	InitWindow (WINDOW_WIDTH, WINDOW_HEIGHT, "Ant -- vs-123");
 	SetTargetFPS (30);
 
 	bool board[BOARD_COLS * BOARD_ROWS] = { 0 };
-	ant_t ants[ANTS_COUNT] = {0};
+	struct ant_t ants[ANTS_COUNT]              = { 0 };
 
+	for (int i = 0; i < ANTS_COUNT; i++)
+		{
+			ants[i].x   = ystar_between (&seed, 0, BOARD_COLS);
+			ants[i].y   = ystar_between (&seed, 0, BOARD_ROWS);
+			ants[i].direction = (enum direction_t) (ystar_between (&seed, 0, 4));
+		}
 
 	while (!WindowShouldClose ())
 		{
 			BeginDrawing ();
-			ClearBackground (DARKGREEN);
+			ClearBackground (RAYWHITE);
+			DrawText ("Press ESC to quit", 10, WINDOW_HEIGHT - 20, 10, DARKGRAY);
 			EndDrawing ();
 		}
 
