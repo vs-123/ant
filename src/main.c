8520e6c8553c6b5b756ee2a8411ab51d3fde14e1
VS-123 <>
2026-03-27 09:18:18 +0000

    main.c: move to struct-oriented paradigm

--- src/main.c
+++ src/main.c
@@ -29,54 +29,72 @@ struct ant_t
 	enum direction_t direction;
 };
 
+struct game_t
+{
+	bool board[BOARD_COLS * BOARD_ROWS];
+	struct ant_t ants[ANTS_COUNT];
+	uint64_t seed;
+};
+
+void
+game_init (struct game_t *game)
+{
+	game->seed = time (NULL);
+	for (int i = 0; i < ANTS_COUNT; i++)
+		{
+			game->ants[i].x = ystar_between (&game->seed, 0, BOARD_COLS);
+			game->ants[i].y = ystar_between (&game->seed, 0, BOARD_ROWS);
+			game->ants[i].direction
+			    = (enum direction_t) (ystar_between (&game->seed, 0, 4));
+		}
+}
+
+void
+game_render (struct game_t *game)
+{
+	ClearBackground (RAYWHITE);
+
+	for (int y = 0; y < BOARD_ROWS; y++)
+		{
+			for (int x = 0; x < BOARD_COLS; x++)
+				{
+					if (game->board[y * BOARD_COLS + x])
+						{
+							DrawRectangle (x * CELL_SIZE,
+							               y * CELL_SIZE,
+							               CELL_SIZE,
+							               CELL_SIZE,
+							               BLACK);
+						}
+				}
+		}
+
+	for (int i = 0; i < ANTS_COUNT; i++)
+		{
+			DrawRectangle (game->ants[i].x * CELL_SIZE,
+			               game->ants[i].y * CELL_SIZE,
+			               CELL_SIZE,
+			               CELL_SIZE,
+			               RED);
+		}
+
+	DrawText ("Press ESC to quit", 10, WINDOW_HEIGHT - 20, 10, DARKGRAY);
+}
+
 int
 main (void)
 {
-	uint64_t seed = time (NULL);
 
 	InitWindow (WINDOW_WIDTH, WINDOW_HEIGHT, "Ant -- vs-123");
 	SetTargetFPS (30);
 
-	bool board[BOARD_COLS * BOARD_ROWS] = { 0 };
-	struct ant_t ants[ANTS_COUNT]       = { 0 };
-
-	for (int i = 0; i < ANTS_COUNT; i++)
-		{
-			ants[i].x         = ystar_between (&seed, 0, BOARD_COLS);
-			ants[i].y         = ystar_between (&seed, 0, BOARD_ROWS);
-			ants[i].direction = (enum direction_t) (ystar_between (&seed, 0, 4));
-		}
+	struct game_t game = { 0 };
+	game_init (&game);
 
 	while (!WindowShouldClose ())
 		{
 			BeginDrawing ();
-			ClearBackground (RAYWHITE);
-			DrawText ("Press ESC to quit", 10, WINDOW_HEIGHT - 20, 10, DARKGRAY);
-
-			for (int y = 0; y < BOARD_ROWS; y++)
-				{
-					for (int x = 0; x < BOARD_COLS; x++)
-						{
-							if (board[y * BOARD_COLS + x])
-								{
-									DrawRectangle (x * CELL_SIZE,
-									               y * CELL_SIZE,
-									               CELL_SIZE,
-									               CELL_SIZE,
-									               BLACK);
-								}
-						}
-				}
-
-			for (int i = 0; i < ANTS_COUNT; i++)
-				{
-					DrawRectangle (ants[i].x * CELL_SIZE,
-					               ants[i].y * CELL_SIZE,
-					               CELL_SIZE,
-					               CELL_SIZE,
-					               RED);
-				}
-
+			game_render (&game);
 			EndDrawing ();
 		}
 
