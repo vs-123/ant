45eb3bda4bd6c0dd69cea77f6ae0bd5ca3521c62
VS-123 <>
2026-03-27 09:01:09 +0000

    main.c: initialise raylib window

--- src/main.c
+++ src/main.c
@@ -1,9 +1,20 @@
 #include <raylib.h>
 #include <stdio.h>
 
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 800
+
 int
 main (void)
 {
-	printf ("hello langton's ant!\n");
+	InitWindow (WINDOW_WIDTH, WINDOW_HEIGHT, "Ant -- vs-123");
+
+	while (!WindowShouldClose ())
+		{
+			BeginDrawing ();
+			ClearBackground (DARKGREEN);
+			EndDrawing ();
+		}
+
 	return 0;
 }
